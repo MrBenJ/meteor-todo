3c7782c963380934207f0224ac0a8b6e5f21cc83
Ben Junya <bjunya@gmail.com>
2015-02-08 10:49:08 -0800

    Added task events to js.

--- simple-todos.js
+++ simple-todos.js
@@ -1,10 +1,43 @@
+
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // Code here runs only on client
   Template.body.helpers({
-    tasks: [
-    { text: "This is task 1"},
-    { text: "This is task 2"},
-    { text: "This is task 3"},
-    ]
+    tasks: function () {
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
   });
+
+  Template.body.events({
+
+    "submit .new-task": function(event) {
+      // Called when new task is submitted (enter key)
+
+       var text = event.target.text.value;
+
+        Tasks.insert({
+          text: text,
+          createdAt: new Date() // Current time
+        });
+
+        // Clear the form
+        event.target.text.value = "";
+
+        // Prevent default form submit
+        return false;
+    }
+  });
+
+  Template.task.events({
+    "click .toggle-checked": function() {
+      Tasks.update(this._id, {$set: {checked: ! this.checked}});
+    },
+
+    "click .delete": function() {
+      Tasks.remove(this._id);
+    }
+  });
+
+
 }

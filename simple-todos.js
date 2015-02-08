d9c09624dcdef66d84d265bca87410a9dc75d74d
Ben Junya <bjunya@gmail.com>
2015-02-08 12:46:49 -0800

    Added Android and iOS platforms. Added filter and count helper classes

--- simple-todos.js
+++ simple-todos.js
@@ -1,12 +1,28 @@
-
 Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // Code here runs only on client
   Template.body.helpers({
+    
     tasks: function () {
-      return Tasks.find({}, {sort: {createdAt: -1}});
-    }
+      if (Session.get("hideCompleted")) {
+        // If hide completed is checked, filter the tasks
+        return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+      }
+      else {
+        // Otherwise, just return all the tasks
+        return Tasks.find({}, {sort: {createdAt: -1}});
+      }
+    },
+
+    hideCompleted: function () {
+      return Session.get("hideCompleted");
+    },
+
+    incompleteCount: function() {
+      return Tasks.find({checked: {$ne: true}}).count();
+    },
+
   });
 
   Template.body.events({
@@ -16,17 +32,30 @@ if (Meteor.isClient) {
 
        var text = event.target.text.value;
 
+       if (text === "") {
+          // Do Nothing
+       }
+
+       else {
         Tasks.insert({
           text: text,
           createdAt: new Date() // Current time
+        
         });
+
+      }
 
         // Clear the form
         event.target.text.value = "";
 
         // Prevent default form submit
         return false;
-    }
+  },
+
+  "change .hide-completed input": function (event) {
+    Session.set("hideCompleted", event.target.checked);
+  }
+
   });
 
   Template.task.events({
